c7fb788fd55776c90a77104e3ae169bc995a6047
001097743 <Qian.ZHONG@student.tafesa.edu.au>
2025-02-15 09:12:08 +1030

    Part 1 #3, #4

--- Student.cs
+++ Student.cs
@@ -14,20 +14,43 @@ namespace PRG547ASSIGNMENT1
         const string DEF_PROGRAM = "Not provided";
         const string DEF_DATE_REGISTERED = "Not provided";
 
+        /// <summary>
+        /// Getters and setters
+        /// https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/properties
+        /// </summary>
         public string StudentID { get; set; }
         public string Program { get; set; }
         public string DateRegistered { get; set; }
 
+        /// <summary>
+        /// All arg constructor
+        /// </summary>
+        /// <param name="studentID">Student ID</param>
+        /// <param name="program">Program</param>
+        /// <param name="dateRegistered">Date Registered</param>
         public Student(string studentID, string program, string dateRegistered)
         {
             StudentID = studentID;
             Program = program;
             DateRegistered = dateRegistered;
         }
-
+        /// <summary>
+        /// No arg constructor
+        /// </summary>
         public Student() : this(DEF_STUDENT_ID, DEF_PROGRAM, DEF_DATE_REGISTERED) { }
 
+        /// <summary>
+        /// Override ToString()
+        /// </summary>
+        /// <returns>String</returns>
         public override string ToString() { return $"Student ID: {StudentID};\nProgram: {Program};\nDate Registered: {DateRegistered};"; }
+
+        /// <summary>
+        /// Override Equals()
+        /// https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/override
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns>bool</returns>
         public override bool Equals(object obj)
         {
             if (obj == null) return false;
@@ -36,9 +59,24 @@ namespace PRG547ASSIGNMENT1
             return obj.GetType() == this.GetType() && student.StudentID == StudentID;
         }
 
+        /// <summary>
+        /// Overload operators ==, !=
+        /// https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/operators/operator-overloading
+        /// </summary>
+        /// <param name="a">Object for </param>
+        /// <param name="b"></param>
+        /// <returns>bool</returns>
         public static bool operator ==(Student a, Student b) { return a.Equals(b); }
-        public static bool operator !=(Student a, Student b) { return !(a == b); }
-        //public static bool operator !=(Student a, Student b) { return !a.Equals(b); }
+        public static bool operator !=(Student a, Student b)
+        {
+            return !(a == b);
+            // same??? !a.Equals(b);
+        }
+
+        /// <summary>
+        /// Override GetHashCode()
+        /// </summary>
+        /// <returns>int</returns>
         public override int GetHashCode() { return StudentID.GetHashCode(); }
 
     }
