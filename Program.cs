cf884d9c4c7484519fe0293048abe6a27d3b1f91
001097743 <Qian.ZHONG@student.tafesa.edu.au>
2025-02-15 08:19:41 +1030

    Part 1 #1, #2

---
+++ Program.cs
@@ -0,0 +1,94 @@
+﻿using PRG547ASSIGNMENT1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#pragma warning disable
+
+namespace PRG547ASSIGNMENT1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("[testing Student]");
+
+            Console.WriteLine();
+            Console.WriteLine("[testing getters, getters, ToString, GetHashCode]");
+
+            Console.WriteLine();
+            Console.WriteLine("> create object: studentNoArg");
+            Student studentNoArg = new Student();
+
+            Console.WriteLine();
+            Console.WriteLine("> ToString()");
+            Console.WriteLine(studentNoArg.ToString());
+
+            Console.WriteLine();
+            Console.WriteLine("> getters");
+            Console.WriteLine($"StudentID: {studentNoArg.StudentID}");
+            Console.WriteLine($"Program: {studentNoArg.Program}");
+            Console.WriteLine($"DateRegister: {studentNoArg.DateRegistered}");
+            Console.WriteLine($"GetHashCode: {studentNoArg.GetHashCode()}");
+
+            Console.WriteLine();
+            Console.WriteLine("> setters: set studentNoArg with (The Student, The Program, The Date)");
+            studentNoArg.StudentID = "The Student";
+            studentNoArg.Program = "The Program";
+            studentNoArg.DateRegistered = "The Date";
+            Console.WriteLine($"StudentID: {studentNoArg.StudentID}");
+            Console.WriteLine($"Program: {studentNoArg.Program}");
+            Console.WriteLine($"DateRegister: {studentNoArg.DateRegistered}");
+            Console.WriteLine($"GetHashCode: {studentNoArg.GetHashCode()}");
+
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine();
+
+            Console.WriteLine("> now the studentNoArg is {The Student, The Program, The Date}");
+            Console.WriteLine();
+            Console.WriteLine("> create object: studentAllArg (The Other Student, The Other Program, The Other Date)");
+            Console.WriteLine();
+            Student studentAllArg = new Student("The Other Student", "The Other Program", "The Other Date");
+            Console.WriteLine(studentAllArg.ToString());
+
+            Console.WriteLine();
+            Console.WriteLine("> create object same as ALL ARG: studentAllArgSame (The Other Student, The Other Program, The Other Date)");
+            Console.WriteLine();
+            Student studentAllArgSame = new Student("The Other Student", "The Other Program", "The Other Date");
+            Console.WriteLine(studentAllArgSame.ToString());
+
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine();
+
+            Console.WriteLine("[testing Equals, ==, !=]");
+
+            Console.WriteLine();
+            Console.WriteLine("> studentAllArg should be the same as studentAllArgSame");
+            Console.WriteLine();
+            Console.WriteLine($"> studentAllArg Equals studentAllArgSame (TRUE): {studentAllArg.Equals(studentAllArgSame)}");
+            Console.WriteLine($"> studentAllArg == studentAllArgSame (TRUE): {studentAllArg == studentAllArgSame}");
+            Console.WriteLine($"> studentAllArg != studentAllArgSame (FALSE): {studentAllArg != studentAllArgSame}");
+
+            Console.WriteLine();
+            Console.WriteLine("> studentNoArg should NOT be the same as studentAllArg");
+            Console.WriteLine();
+            Console.WriteLine($"> studentNoArg Equals studentAllArg (FALSE): {studentNoArg.Equals(studentAllArg)}");
+            Console.WriteLine($"> studentNoArg == studentAllArg (FALSE): {studentNoArg == studentAllArg}");
+            Console.WriteLine($"> studentNoArg != studentAllArg (TRUE): {studentNoArg != studentAllArg}");
+
+
+
+
+
+
+
+
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Runtime.Remoting.Messaging;
+
+namespace PRG547ASSIGNMENT1
+{
+    internal class Student
+    {
+        const string DEF_STUDENT_ID = "Not provided";
+        const string DEF_PROGRAM = "Not provided";
+        const string DEF_DATE_REGISTERED = "Not provided";
+
+        public string StudentID { get; set; }
+        public string Program { get; set; }
+        public string DateRegistered { get; set; }
+
+        public Student(string studentID, string program, string dateRegistered)
+        {
+            StudentID = studentID;
+            Program = program;
+            DateRegistered = dateRegistered;
+        }
+
+        public Student() : this(DEF_STUDENT_ID, DEF_PROGRAM, DEF_DATE_REGISTERED) { }
+
+        public override string ToString() { return $"Student ID: {StudentID};\nProgram: {Program};\nDate Registered: {DateRegistered};"; }
+        public override bool Equals(object obj)
+        {
+            if (obj == null) return false;
+            if (ReferenceEquals(obj, this)) return true;
+            Student student = (Student)obj;
+            return obj.GetType() == this.GetType() && student.StudentID == StudentID;
+        }
+
+        public static bool operator ==(Student a, Student b) { return a.Equals(b); }
+        public static bool operator !=(Student a, Student b) { return !(a == b); }
+        //public static bool operator !=(Student a, Student b) { return !a.Equals(b); }
+        public override int GetHashCode() { return StudentID.GetHashCode(); }
+
+    }
+}
